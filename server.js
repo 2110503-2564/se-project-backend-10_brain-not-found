be7d05540c28eb00eef32fcc9a0d70c89fcfa91f
TeeRatTaPon27 <teerattapon080849@gmail.com>
2025-03-22 14:48:07 +0700

    Increase rate limit

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use(xss());
 //Rate Limiting
 const limiter=rateLimit({
     windowsMs:10*60*1000,//10 mins
-    max: 100
+    max: 25000
 });
 app.use(limiter);
 //Prevent http param pollutions
