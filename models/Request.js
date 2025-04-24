41fbb67e7de011fccb8f71e852d5a17abf1c070e
SatanAlphabet <ori0pppppp@gmail.com>
2025-04-24 15:15:47 +0700

    edit request schema to use nested shop schema

--- models/Request.js
+++ models/Request.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Shop = require('./Shop')
 
 const RequestSchema = new mongoose.Schema({
     user: {
@@ -10,68 +11,13 @@ const RequestSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     },
-    address:{
-        type: String,
-        required: [true, 'Please add an address\n']
+    shop: { 
+        // Directly imports the shop schema to be used here
+        // Make edits on the shop schema instead
+        type: Shop,
+        required: true
     },
-    district:{
-        type: String,
-        required: [true, 'Please add a district\n']
-    },
-    province:{
-        type: String,
-        required: [true, 'Please add a province\n']
-    },
-    postalcode:{
-        type: String,
-        required: [true, 'Please add a postalcode\n'], maxlength:[5, 'Postal Code can not be more than 5 digits\n']
-    },
-    tel: {
-        type: String,
-        required: [true, 'Please add a phone number\n'],
-        match: [
-            /^(\d{3}-?\d{3}-?\d{4})$/, 
-            'Please add a valid phone number\n'
-        ]
-    },
-    region:{
-        type: String,
-        required: [true, 'Please add a region\n']
-    },
-    openTime: {
-        type: String,
-        required: [true, 'Please add an open time\n'],
-        match: [/^(0[0-9]|1[0-9]|2[0-3]):([0-5][0-9])$/, 'Please add a valid open time in HH:MM format\n']
-    },
-    closeTime: {
-        type: String,
-        required: [true, 'Please add a close time\n'],
-        match: [/^(0[0-9]|1[0-9]|2[0-3]):([0-5][0-9])$/, 'Please add a valid close time in HH:MM format\n']
-    },
-    picture: {
-        type: [String], // เป็น array ของ string
-        validate: [
-            {
-                validator: function(arr) {
-                    return arr.length <= 5; // จำกัดไม่เกิน 5 รูป
-                },
-                message: 'You can upload up to 5 pictures only\n'
-            }
-        ],
-        required: [true, 'Please add at least 1 picture\n']
-    },
-    shopType: {
-        type: String,
-        required: false
-    },
-    services: {
-        type: [String],
-        required: false
-    },
-    licenseNumber: {
-        type: String,
-        required: false
-    },
+    
     status: {
         type: String,
         enum: ['pending', 'approved', 'rejected'],
