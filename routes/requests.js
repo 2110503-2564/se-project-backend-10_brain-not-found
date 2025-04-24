9ce76e6aa8123408a1a3bbc69bc13348f8d85b8a
SatanAlphabet <ori0pppppp@gmail.com>
2025-04-24 15:14:19 +0700

    merge router files

--- routes/requests.js
+++ routes/requests.js
@@ -2,12 +2,13 @@ const express = require('express');
 const { createRequest , getRequests } = require('../controllers/request');
 const router = express.Router();
 const {protect,authorize} = require('../middleware/auth');
-const { create } = require('../models/User');
-const { get } = require('mongoose');
 
 
 router.route('/')
     .get(protect,authorize('admin','shopOwner'),getRequests)
     .post(protect,authorize('shopOwner'),createRequest);
+
+router.route('/:id/approve').post(protect,authorize('admin'),approveShop);
+router.route('/:id/reject').post(protect,authorize('admin'),rejectShop);
 
 module.exports = router;
